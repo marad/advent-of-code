94cf863f61b48d14c9f50f5eefc0a90e0a3ffc30
Marcin Radoszewski <moriturius@gmail.com>
2019-12-12 21:32:05 +0100

    day 10 (wip)

--- 2019/bin/day10.dart
+++ 2019/bin/day10.dart
@@ -1,4 +1,10 @@
-var map = """
+import 'package:aoc2019/src/day10.dart';
+import 'package:collection/collection.dart';
+import 'package:decimal/decimal.dart';
+import 'dart:math';
+import 'dart:core';
+
+var desc = """
 ....#.....#.#...##..........#.......#......
 .....#...####..##...#......#.........#.....
 .#.#...#..........#.....#.##.......#...#..#
@@ -42,8 +48,40 @@ var map = """
 ...#....#.#..#.........#.......#..#...##...
 ...##..............#......#................
 ........................#....##..#........#
-""".trim();
+""".trimLines();
+
+class AsteroidInfo {
+  final Asteroid asteroid;
+  final int distance; // distance from monitoring station
+  final double angle;
+
+  AsteroidInfo(this.asteroid, this.distance, this.angle);
+
+  factory AsteroidInfo.from(Asteroid monitoringStation, Asteroid target) {
+    var distance = (monitoringStation.x - target.x).abs() + (monitoringStation.y - target.y).abs();
+    var top = Asteroid(monitoringStation.x, monitoringStation.y - 1);
+    var angle = getAngle(target, monitoringStation, top);
+
+    return AsteroidInfo(target, distance, angle < 0 ? angle + pi * 2 : angle);
+  }
+
+}
+
+// TODO: group and sort by angle, then by distance
+// for each laser rotation it destroys first asteroid in each group
+
+double getAngle(Asteroid a, Asteroid b, Asteroid c) {
+  return atan2(c.y - b.y, c.x - b.x) - atan2(a.y-b.y, a.x-b.x);
+}
 
 void main() {
-  
+  var map = Map.fromDesc(desc);
+  var result = findBesetAsteroid(map);
+  print('Best asteroid: ${result.asteroid}, ${result.visibleCount} others visible');
+
+
+  var monitoringStation = Asteroid(30,34);
+  var asteroids = map.asteroids.map((it) => AsteroidInfo.from(monitoringStation, it)).toList();
+  var grouped = groupBy(asteroids, (AsteroidInfo info) => info.angle);
+  print(grouped.keys);
 }
--- 2019/test/day10_test.dart
+++ 2019/test/day10_test.dart
@@ -1,65 +1,56 @@
 import 'package:test/test.dart';
+import 'package:aoc2019/src/day10.dart';
 
-class Asteroid {
-  int _x;
-  int get x => _x;
-  int _y;
-  int get y => _y;
 
-  Asteroid(this._x, this._y);
-
-  @override
-  String toString() {
-    return "($_x, $_y)";
-  }
-}
-
-class Map {
-  List<Asteroid> _asteroids;
-  List<Asteroid> get asteroids => _asteroids;
-  Map(this._asteroids);
-
-  factory Map.from(String mapDescription) {
-    int asteroidChar = "#".codeUnitAt(0);
-    List<Asteroid> asteroids = [];
-    var lines = mapDescription.split("\n");
-    for(var y=0; y < lines.length; y++) {
-      var line = lines[y];
-      for(var x=0; x < line.length; x++) {
-        var char = line.codeUnitAt(x);
-        if (char == asteroidChar) {
-          asteroids.add(Asteroid(x, y));
-        }
-      }
-    }
-    return Map(asteroids);
-  }
-
-  @override
-  String toString() {
-    return _asteroids.toString();
-  }
-}
 
 void main() {
   test("test case 1", () {
-    var map = """
+    var map = Map.fromDesc("""
       .#..#
       .....
       #####
       ....#
       ...##
-      """.trimLines();
-
-    print(Map.from(map));
+      """.trimLines());
+    
+    var result = findBesetAsteroid(map);
+    expect(result.asteroid, Asteroid(3, 4));
+    expect(result.visibleCount, 8);
   });
-}
 
-extension Trimmer on String {
-  String trimLines() {
-    return this
-      .replaceAll(RegExp(r"\n\s+"), "\n")
-      .replaceAll(RegExp(r"\s+\n"), "\n")
-      .trim();
-  }
+  test("test case 2", () {
+    var map = Map.fromDesc("""
+      ......#.#.
+      #..#.#....
+      ..#######.
+      .#.#.###..
+      .#..#.....
+      ..#....#.#
+      #..#....#.
+      .##.#..###
+      ##...#..#.
+      .#....####""".trimLines());
+    
+    var result = findBesetAsteroid(map);
+    expect(result.asteroid, Asteroid(5, 8));
+    expect(result.visibleCount, 33);
+  });
+
+  test("test case 3", () {
+    var map = Map.fromDesc("""
+      #.#...#.#.
+      .###....#.
+      .#....#...
+      ##.#.#.#.#
+      ....#.#.#.
+      .##..###.#
+      ..#...##..
+      ..##....##
+      ......#...
+      .####.###.""".trimLines());
+    
+    var result = findBesetAsteroid(map);
+    expect(result.asteroid, Asteroid(1, 2));
+    expect(result.visibleCount, 35);
+  });
 }
